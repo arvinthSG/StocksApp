42ec3786e0b7cd2f69e191c22819e31afed23a6a
Arvinthan Sundaram <arvinth.sg@gmail.com>
2022-10-14 00:49:30 -0700

    updated stock ticker view and colors

--- app/src/main/java/com/example/stocksapp/Adapter/PortfolioListAdapter.kt
+++ app/src/main/java/com/example/stocksapp/Adapter/PortfolioListAdapter.kt
@@ -1,21 +1,31 @@
 package com.example.stocksapp.Adapter
 
+import android.content.Context
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stocksapp.Data.Stock
 import com.example.stocksapp.R
 
-class PortfolioListAdapter(private val data: List<Stock>): RecyclerView.Adapter<PortfolioListAdapter.ViewHolder>()  {
+class PortfolioListAdapter(private val data: List<Stock>) :
+    RecyclerView.Adapter<PortfolioListAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    private lateinit var context: Context
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvStockTicker: TextView = itemView.findViewById(R.id.tv_stock_ticker)
         val tvStockName: TextView = itemView.findViewById(R.id.tv_stock_name)
+        val tvCurrency: TextView = itemView.findViewById(R.id.tv_currency)
+        val tvPrice: TextView = itemView.findViewById(R.id.tv_currency_price)
+        val tvQuantity: TextView = itemView.findViewById(R.id.tv_quantity)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        this.context = parent.context
         return ViewHolder(
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.portfolio_list_item, parent, false)
@@ -26,6 +36,18 @@ class PortfolioListAdapter(private val data: List<Stock>): RecyclerView.Adapter<
         val stockItem = data[position]
         viewHolder.tvStockTicker.text = stockItem.ticker
         viewHolder.tvStockName.text = stockItem.name
+        if (stockItem.currency.isNullOrEmpty()) {
+            viewHolder.tvCurrency.visibility = GONE
+        } else {
+            viewHolder.tvCurrency.text = stockItem.currency
+        }
+        viewHolder.tvPrice.text = (stockItem.currentPriceCents / 100.00).toString()
+        if (stockItem.quantity == null) {
+            viewHolder.tvQuantity.visibility = GONE
+        } else {
+            viewHolder.tvQuantity.text =
+                context.getString(R.string.quantity, stockItem.quantity.toString())
+        }
 
     }
 
